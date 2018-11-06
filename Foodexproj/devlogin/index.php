35c9a05353edcf4c070c898acb2d3a88ad90099b
gatsjanos <gatsjanos@gmail.com>
2018-11-06 23:40:19 +0100

    DevLogin mukodik

---
+++ Foodexproj/devlogin/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Developer login</title>
+</head>
+<body>
+<form action="devlogin.php" method="POST" style="width: 100%">
+    <p>Login as:</p>
+    <input type="text" name="int_id" id="int_id" placeholder="internal_id" style="width: 95%">
+
+    <p>Password:</p>
+    <input type="password" name="password" id="password" placeholder="********">
+    <br>
+    <button type="submit">Developer login</button>
+</form>
+
+<script>
+    var IntIdField = document.getElementById('int_id');
+</script>
+
+<table>
+
+    <?php
+    require_once '../Eszkozok/Eszk.php';
+
+    try
+    {
+    $conn = \Eszkozok\Eszk::initMySqliObject();
+    if (!$conn)
+        throw new \Exception('$conn is false!');
+
+    $stmt = $conn->prepare("SELECT `nev`, `internal_id` FROM `fxaccok` ORDER BY `nev` ASC ;");
+
+    if (!$stmt)
+        throw new \Exception('$stmt is false!');
+
+    if ($stmt->execute())
+    {
+    $result = $stmt->get_result();
+
+    $row;
+    do
+    {
+    ?>
+    <tr>
+        <?php
+        $i = 0;
+        while ($row = $result->fetch_assoc())
+        {
+            ?>
+            <td style="cursor: pointer; color: #5bc0de" onclick="IntIdField.value = '<?php echo $row['internal_id'] ?>';"><?php echo $row['nev']; ?></td>
+            <?php
+            ++$i;
+
+            if ($i >= 3)
+                break;
+        }
+        ?>
+
+    <tr>
+        <?php
+        }while ($row);
+
+        }
+        else
+        {
+            throw new \Exception('$stmt->execute() is false!');
+        }
+
+        }
+        catch (\Exception $e)
+        {
+            ob_clean();
+            die('Error: ' . $e->getMessage());
+        }
+
+        ?>
+
+</table>
+
+</body>
+</html>
