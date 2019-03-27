f8e076ebd1ac8e9ad63b5a2615c0b1feff35b662
gatsjanos <gatsjanos@gmail.com>
2019-03-27 16:57:02 +0100

    Devlogin jelszo hashelve tarolva

--- Foodexproj/devlogin/devlogin.php
+++ Foodexproj/devlogin/devlogin.php
@@ -5,12 +5,14 @@ require_once '../Eszkozok/Eszk.php';
 require_once '../Eszkozok/param.php';
 require_once '../Eszkozok/ini.php';
 
+//die(sha1(GetURLParam('password')));
+
 if (\Eszkozok\GlobalServerInitParams::$DevloginEnabled)
 {
 
     if (IsURLParamSet('int_id') && IsURLParamSet('password'))
     {
-        if (GetURLParam('password') == \Eszkozok\GlobalServerInitParams::$DevloginPassword)
+        if (sha1(GetURLParam('password')) == \Eszkozok\GlobalServerInitParams::$DevloginPasswordHashed)
         {
             $_SESSION['profilint_id'] = GetURLParam('int_id');
             \Eszkozok\Eszk::RedirectUnderRoot('profil');
