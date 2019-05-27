48a945fc3875de8b0d582bb66970067766c904be
Dzsani <gatsjanos@gmail.com>
2019-05-27 22:49:07 +0200

    database structure refreshed in source

--- database.sql
+++ database.sql
@@ -3,20 +3,24 @@
 SET NAMES utf8;
 SET time_zone = '+00:00';
 
+DROP TABLE IF EXISTS `fxaccok`;
 CREATE TABLE `fxaccok` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `internal_id` varchar(120) CHARACTER SET latin2 COLLATE latin2_hungarian_ci DEFAULT NULL COMMENT 'AuthSch internal_id',
   `nev` text CHARACTER SET latin2 COLLATE latin2_hungarian_ci COMMENT 'AuthSch DisplayName',
-  `ujmuszakjog` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Új műszak kiírására jogosult (0/1)',
-  `adminjog` tinyint(4) NOT NULL,
+  `fxtag` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Fx kortag PEK szerint',
+  `adminjog` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Admin',
+  `muszjeljog` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Muszakra jelentkezhet',
+  `pontlatjog` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Lathatja masok pontszamat',
   `email` text CHARACTER SET latin2 COLLATE latin2_hungarian_ci,
   `session_token` varchar(124) CHARACTER SET latin2 COLLATE latin2_hungarian_ci DEFAULT NULL COMMENT 'Bejelentkezett session token',
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID` (`ID`),
   UNIQUE KEY `internal_id` (`internal_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 
+DROP TABLE IF EXISTS `fxjelentk`;
 CREATE TABLE `fxjelentk` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `jelentkezo` varchar(120) CHARACTER SET latin2 COLLATE latin2_hungarian_ci NOT NULL COMMENT 'A jelentkezo ember internal_id-je ',
@@ -28,13 +32,15 @@ CREATE TABLE `fxjelentk` (
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID` (`ID`),
   UNIQUE KEY `ID_2` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=606 DEFAULT CHARSET=latin1;
 
 
+DROP TABLE IF EXISTS `fxmuszakok`;
 CREATE TABLE `fxmuszakok` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `kiirta` varchar(120) CHARACTER SET latin2 COLLATE latin2_hungarian_ci DEFAULT NULL COMMENT 'A muszak kiirojanak internal_id-je',
   `musznev` varchar(250) CHARACTER SET latin2 COLLATE latin2_hungarian_ci NOT NULL,
+  `korID` int(11) DEFAULT NULL,
   `aktiv` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0: inaktiv, 1: aktiv',
   `idokezd` datetime DEFAULT NULL,
   `idoveg` datetime DEFAULT NULL,
@@ -42,10 +48,13 @@ CREATE TABLE `fxmuszakok` (
   `pont` float NOT NULL DEFAULT '2',
   `mospont` float NOT NULL DEFAULT '0.5',
   `megj` varchar(250) CHARACTER SET latin2 COLLATE latin2_hungarian_ci NOT NULL COMMENT 'megjegyzes',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`ID`),
+  KEY `korID` (`korID`),
+  CONSTRAINT `fxmuszakok_ibfk_1` FOREIGN KEY (`korID`) REFERENCES `korok` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=249 DEFAULT CHARSET=latin1;
 
 
+DROP TABLE IF EXISTS `globalsettings`;
 CREATE TABLE `globalsettings` (
   `nev` varchar(200) COLLATE latin2_hungarian_ci NOT NULL,
   `ertek` text COLLATE latin2_hungarian_ci NOT NULL,
@@ -53,6 +62,7 @@ CREATE TABLE `globalsettings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin2 COLLATE=latin2_hungarian_ci;
 
 
+DROP TABLE IF EXISTS `kompenz`;
 CREATE TABLE `kompenz` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `internal_id` varchar(120) CHARACTER SET latin2 COLLATE latin2_hungarian_ci NOT NULL,
@@ -60,9 +70,18 @@ CREATE TABLE `kompenz` (
   `megj` text CHARACTER SET latin2 COLLATE latin2_hungarian_ci NOT NULL COMMENT 'megjegyzes',
   `ido` datetime NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=latin1;
 
 
+DROP TABLE IF EXISTS `korok`;
+CREATE TABLE `korok` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nev` varchar(220) COLLATE latin2_hungarian_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin2 COLLATE=latin2_hungarian_ci;
+
+
+DROP TABLE IF EXISTS `logs`;
 CREATE TABLE `logs` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `datetime` datetime NOT NULL,
@@ -73,17 +92,19 @@ CREATE TABLE `logs` (
   `level_name` text COLLATE latin2_hungarian_ci NOT NULL,
   `extra` text COLLATE latin2_hungarian_ci NOT NULL,
   UNIQUE KEY `ID` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin2 COLLATE=latin2_hungarian_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=891 DEFAULT CHARSET=latin2 COLLATE=latin2_hungarian_ci;
 
 
+DROP TABLE IF EXISTS `pontjeldelay`;
 CREATE TABLE `pontjeldelay` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `minpont` float NOT NULL,
   `delay` int(11) NOT NULL COMMENT 'sec',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin2 COLLATE=latin2_hungarian_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin2 COLLATE=latin2_hungarian_ci;
 
 
+DROP TABLE IF EXISTS `profilinfo`;
 CREATE TABLE `profilinfo` (
   `int_id` varchar(120) COLLATE latin2_hungarian_ci NOT NULL,
   `kedv_vicc` text COLLATE latin2_hungarian_ci COMMENT 'Kedvenc vicc',
@@ -91,4 +112,4 @@ CREATE TABLE `profilinfo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin2 COLLATE=latin2_hungarian_ci;
 
 
--- 2019-05-10 18:51:18
+-- 2019-05-27 20:42:26
